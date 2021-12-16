93f06bebc20ee6b4f758cf54f01c8975263a770c
github username <github email address>
2021-12-16 15:35:16 -0500

    add unit tests for new functionality

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -38,4 +38,86 @@ describe('ListComponent', () => {
       expect(component.selectedProviders.length).toEqual(0);
     });
   });
+
+  describe('selectProvider', () => {
+
+    it('should add a provider to selectedProviders', () => {
+      const provider = {
+          id: '1',
+          name: 'Peter',
+          address: '111 Valley Rd',
+          phone: '1234567'
+      };
+
+      component.selectProvider(provider);
+
+      expect(component.selectedProviders.length).toEqual(1);
+    });
+});
+
+describe('removeProvider', () => {
+
+    it('should remove a provider from selectedProviders', () => {
+      component.selectedProviders = [
+          {
+            id: '1',
+            name: 'Peter',
+            address: '111 Valley Rd',
+            phone: '1234567'
+          }
+      ];
+
+      const provider = component.selectedProviders[0];
+
+      component.removeProvider(provider);
+
+      expect(component.selectedProviders.length).toEqual(0);
+    });
+});
+
+describe('saveLocally', () => {
+
+    it('should save data into local storage', () => {
+      const localStore = {};
+      component.selectedProviders = [];
+      component.unselectedProviders = [
+        {
+          id: '1',
+          name: 'John',
+          address: '123 Greenway Blvd',
+          phone: '8991234321'
+        },
+        {
+          id: '2',
+          name: 'Mary',
+          address: '443 Windwhisper Road',
+          phone: '2233211903'
+        },
+        {
+          id: '3',
+          name: 'Jason',
+          address: '9992 Pumpkin Hollow',
+          phone: '4343219384'
+        }
+      ];
+
+      spyOn(localStorage, 'setItem').and.callFake(function (key, value) {
+          return localStore[key] = value;
+      });
+
+      spyOn(localStorage, 'getItem').and.callFake(function (key) {
+          return localStore[key];
+      });
+
+      component.saveProviderLists();
+
+      const local_selectedProviders   = JSON.parse(localStorage.getItem('selectedProviders'));
+      const local_unselectedProviders = JSON.parse(localStorage.getItem('unselectedProviders'));
+
+      expect(local_selectedProviders.length).toEqual(component.selectedProviders.length);
+      expect(local_unselectedProviders.length).toEqual(component.unselectedProviders.length);
+
+    });
+});
+
 });
